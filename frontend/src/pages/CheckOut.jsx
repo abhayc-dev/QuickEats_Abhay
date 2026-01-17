77c3d5001d6099af83dfec5772a9c1c7c1d170d0
Abhay Chaurasia <118441634+abhay2321@users.noreply.github.com>
2026-01-18 02:21:14 +0530

    Fix missing Leaflet marker icon in production

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -9,6 +9,17 @@ import { GoSearch } from "react-icons/go";
 import { useDispatch, useSelector } from "react-redux";
 import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 import { setAddress, setLocation } from "../redux/mapSlice";
 import axios from "axios";
 import { FcMoneyTransfer } from "react-icons/fc";
